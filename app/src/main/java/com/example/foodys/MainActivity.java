132c6a3098b4faa4d63986d17e31ae4e052b4206
Naren <mnaren0010@gmail.com>
2021-03-31 21:02:18 +0530

    2nd

---
+++ app/src/main/java/com/example/foodys/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.foodys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageVieww;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageVieww=(ImageView)findViewById(R.id.imageView);
+        textView=(TextView)findViewById(R.id.textView7);
+        imageVieww.animate().alpha(0f).setDuration(0);
+        textView.animate().alpha(0f).setDuration(0);
+        imageVieww.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
+    }
+}
+
