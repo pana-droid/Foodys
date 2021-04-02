55ce6afa546cd6f0d9b1f0aef6b9d2bf02df78dc
arihant <arihantrajdhariwal@gmail.com>
2021-04-03 01:27:42 +0530

    vid6

---
+++ app/src/main/java/com/example/foodys/ChefRegisteration.java
@@ -0,0 +1,14 @@
+package com.example.foodys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChefRegisteration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_registeration);
+    }
+}
--- app/src/main/java/com/example/foodys/MainActivity.java
+++ app/src/main/java/com/example/foodys/MainActivity.java
@@ -5,20 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
-    ImageView imageVieww;
-    TextView textView;
+
+    ImageView logo,appName,splashImg;
+    LottieAnimationView lottieAnimationView;
+
+    //ImageView imageVieww;
+    //TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageVieww=(ImageView)findViewById(R.id.imageView);
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.img);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
+
+
+        /*imageVieww=(ImageView)findViewById(R.id.imageView);
         textView=(TextView)findViewById(R.id.textView7);
         imageVieww.animate().alpha(0f).setDuration(0);
         textView.animate().alpha(0f).setDuration(0);
@@ -38,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
 
             }
-        },3000);
+        },3000);*/
     }
 }
 
