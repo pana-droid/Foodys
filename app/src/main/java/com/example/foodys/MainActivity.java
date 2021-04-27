88a8fda4340a8a5a5585c317fa350024ee818e85
arihant <arihantrajdhariwal@gmail.com>
2021-04-28 01:25:44 +0530

    vid12

--- app/src/main/java/com/example/foodys/MainActivity.java
+++ app/src/main/java/com/example/foodys/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.foodys;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView logo,appName,splashImg;
+    ImageView logo, appName, splashImg;
     LottieAnimationView lottieAnimationView;
+    FirebaseAuth Fauth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     //ImageView imageVieww;
     //TextView textView;
@@ -35,17 +48,136 @@ public class MainActivity extends AppCompatActivity {
         appName.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
 
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                Fauth = FirebaseAuth.getInstance();
+                if (Fauth.getCurrentUser() != null) {
+                    if (Fauth.getCurrentUser().isEmailVerified()) {
+                        Fauth = FirebaseAuth.getInstance();
+                        databaseReference = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getUid() + "/Role");
+                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                Intent intent = new Intent(MainActivity.this,MainMenu.class);
-                startActivity(intent);
-                finish();
+                                String role = snapshot.getValue(String.class);
+
+                                if (role.equals("Chef")) {
+                                    Intent a = new Intent(MainActivity.this, ChefFoodPanel_BottomNavigation.class);
+                                    startActivity(a);
+                                    finish();
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+
+                    } else {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Check whether you have verified your details, Otherwise please verify");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                dialog.dismiss();
+                                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                        Fauth.signOut();
+
+                    }
+                } else {
+
+
+                    Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+
+        },3000);
 
             }
-        },3000);
+        }
+
+
+
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {*/
+
+                /*Fauth = FirebaseAuth.getInstance();
+                if (Fauth.getCurrentUser() != null) {
+                    if (Fauth.getCurrentUser().isEmailVerified()) {
+                        Fauth = FirebaseAuth.getInstance();
+                        databaseReference = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getUid() + "/Role");
+                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                String role = snapshot.getValue(String.class);
+
+                                if (role.equals("Chef")) {
+                                    Intent a = new Intent(MainActivity.this, ChefFoodPanel_BottomNavigation.class);
+                                    startActivity(a);
+                                    finish();
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+
+                    } else {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("Check whether you have verified your details, Otherwise please verify");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                dialog.dismiss();
+                                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        });
+                        AlertDialog alert = builder.create();
+                        alert.show();
+                        Fauth.signOut();
+
+                    }
+                } else {
+
+
+                    Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+*/
+            ;
+
 
 
         /*imageVieww=(ImageView)findViewById(R.id.imageView);
@@ -69,6 +201,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         },3000);*/
-    }
-}
+
 
