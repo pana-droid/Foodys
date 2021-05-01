fde1bb5528cbea568d57180c4baa0cccaa2f81fa
arihant <arihantrajdhariwal@gmail.com>
2021-05-02 03:01:49 +0530

    vid14

---
+++ app/src/main/java/com/example/foodys/CustomerFoodPanel_BottomNavigation.java
@@ -0,0 +1,14 @@
+package com.example.foodys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_food_panel__bottom_navigation);
+    }
+}
---
+++ app/src/main/java/com/example/foodys/Delivery_FoodPanelBottomNavigation.java
@@ -0,0 +1,14 @@
+package com.example.foodys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Delivery_FoodPanelBottomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery__food_panel_bottom_navigation);
+    }
+}
